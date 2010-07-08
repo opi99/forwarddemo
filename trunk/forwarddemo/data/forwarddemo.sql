9b4234eabef00ac35e0ced268698a8ceb90802a0
opi <opi@329c4359-9da0-4f32-8655-818518c4f8f5>
2010-07-08 23:43:13 +0000

    #32 add sql file

---
+++ trunk/forwarddemo/data/forwarddemo.sql
@@ -0,0 +1,9 @@
+--
+-- Tabellenstruktur für Tabelle `forwarddemo`
+--
+
+CREATE TABLE IF NOT EXISTS `forwarddemo` (
+  `uid` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `value` varchar(255) NOT NULL,
+  PRIMARY KEY (`uid`)
+) AUTO_INCREMENT=1 ;
