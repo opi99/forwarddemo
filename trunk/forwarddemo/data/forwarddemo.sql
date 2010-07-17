3b64e9eb1f5facf4160dad71f2ab2b6140f73195
opi <opi@329c4359-9da0-4f32-8655-818518c4f8f5>
2010-07-17 00:13:04 +0000

    #32 add test data

--- trunk/forwarddemo/data/forwarddemo.sql
+++ trunk/forwarddemo/data/forwarddemo.sql
@@ -7,3 +7,8 @@ CREATE TABLE IF NOT EXISTS `forwarddemo` (
   `value` varchar(255) NOT NULL,
   PRIMARY KEY (`uid`)
 ) AUTO_INCREMENT=1 ;
+
+
+INSERT INTO `forwarddemo` (`uid`, `value`) VALUES
+(1, 'Test 1'),
+(2, 'Test 2');
