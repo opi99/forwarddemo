d8e928b2e00e56010c574f350f948aad00271dec
Alexander Opitz <opitz.alexander@googlemail.com>
2015-09-04 14:39:28 +0000

    Update to use ServiceClass

--- config.php
+++ config.php
@@ -3,13 +3,21 @@
 error_reporting(E_ALL & ~E_NOTICE);
 ini_set('display_errors', true);
 
-require_once '../../forwardfw/config.php';
-
+set_include_path(dirname(__FILE__) . '/../forwardfw/src' . PATH_SEPARATOR . get_include_path());
 set_include_path(dirname(__FILE__) . '/src' . PATH_SEPARATOR . get_include_path());
+set_include_path(dirname(__FILE__) . '/../forwardfw/libs' . PATH_SEPARATOR . get_include_path());
 set_include_path(dirname(__FILE__) . '/libs' . PATH_SEPARATOR . get_include_path());
+
+require_once 'ForwardFW/Autoloader.php';
 
 $GLOBALS['ForwardFW\\Filter\\RequestResponse'] = array(
     (new ForwardFW\Config\Filter\RequestResponse\FirePhp()),
+    (new ForwardFW\Config\Filter\RequestResponse\RegisterServices())
+        ->addService(
+            (new ForwardFW\Config\Service\DataHandler\Mdb2())
+                ->setDsn('mysqli://john:doe@localhost/forwardfw')
+                ->setTablePrefix('')
+        ),
     (new ForwardFW\Config\Filter\RequestResponse\Application())
         ->setConfig(
             (new ForwardFW\Config\Application())
