f8fadb5b26c8f04349a8d2da9fe984b15e576d01
opi <opi@329c4359-9da0-4f32-8655-818518c4f8f5>
2015-09-20 17:03:00 +0000

    Cleanup configuration.

--- trunk/forwarddemo/config.php
+++ trunk/forwarddemo/config.php
@@ -1,40 +1,32 @@
 <?php
 
-error_reporting(E_ALL & ~E_NOTICE);
-ini_set('display_errors', true);
-
-set_include_path(dirname(__FILE__) . '/../forwardfw/src' . PATH_SEPARATOR . get_include_path());
-set_include_path(dirname(__FILE__) . '/src' . PATH_SEPARATOR . get_include_path());
-set_include_path(dirname(__FILE__) . '/../forwardfw/libs' . PATH_SEPARATOR . get_include_path());
-set_include_path(dirname(__FILE__) . '/libs' . PATH_SEPARATOR . get_include_path());
-
-require_once 'ForwardFW/Autoloader.php';
-
-$GLOBALS['ForwardFW\\Filter\\RequestResponse'] = array(
-    (new ForwardFW\Config\Filter\RequestResponse\FirePhp()),
-    (new ForwardFW\Config\Filter\RequestResponse\RegisterServices())
+return (new ForwardFW\Config\Runner())
         ->addService(
             (new ForwardFW\Config\Service\DataHandler\Mdb2())
                 ->setDsn('mysqli://john:doe@localhost/forwardfw')
                 ->setTablePrefix('')
-        ),
-    (new ForwardFW\Config\Filter\RequestResponse\Application())
-        ->setConfig(
-            (new ForwardFW\Config\Application())
-                ->setName('SimpleFormDemo')
-                ->setScreens(
-                    array(
-                        'Input'     => 'ForwardDemo\\Controller\\Screen\\Input',
-                        'Textfield' => 'ForwardDemo\\Controller\\Screen\\Textfield',
-                        'Database'  => 'ForwardDemo\\Controller\\Screen\\Database',
-                        'Cache'     => 'ForwardDemo\\Controller\\Screen\\Cache',
-                    )
-                )
-                ->setIdent('SimpleFormDemo')
-                ->setTemplaterConfig(
-                    (new ForwardFW\Config\Templater\Smarty())
-                        ->setCompilePath(getcwd() . '/../cache/')
-                        ->setTemplatePath(getcwd() . '/../data/templates/smarty')
+        )
+        ->addProcessor(
+            new ForwardFW\Config\Filter\RequestResponse\FirePhp()
+        )
+        ->addProcessor(
+            (new ForwardFW\Config\Filter\RequestResponse\Application())
+                ->setConfig(
+                    (new ForwardFW\Config\Application())
+                        ->setName('SimpleFormDemo')
+                        ->setScreens(
+                            array(
+                                'Input'     => 'ForwardDemo\\Controller\\Screen\\Input',
+                                'Textfield' => 'ForwardDemo\\Controller\\Screen\\Textfield',
+                                'Database'  => 'ForwardDemo\\Controller\\Screen\\Database',
+                                'Cache'     => 'ForwardDemo\\Controller\\Screen\\Cache',
+                            )
+                        )
+                        ->setIdent('SimpleFormDemo')
+                        ->setTemplaterConfig(
+                            (new ForwardFW\Config\Templater\Smarty())
+                                ->setCompilePath(getcwd() . '/../cache/')
+                                ->setTemplatePath(getcwd() . '/../data/templates/smarty')
+                        )
                 )
         )
-);
